16ec71d176106d3d53f1602613377f8014e8d328
Adrien_D <aaaaadrien@users.noreply.github.com>
2014-06-07 10:09:08 +0200

    Add update for index.php

--- update-marjo21.sh
+++ update-marjo21.sh
@@ -6,4 +6,6 @@ cd "$path"
 rm bot.pl
 wget https://raw.githubusercontent.com/aaaaadrien/marjo21/master/bot.pl
 chmod +x bot.pl
- 
+
+rm public_html/index.php
+wget -P public_html/ https://raw.githubusercontent.com/aaaaadrien/marjo21/master/public_html/index.php
