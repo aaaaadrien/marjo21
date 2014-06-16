a1994e5bd473ef8dfe5e8e2c39234c73437a2228
aaaaadrien <aaaaadrien@users.noreply.github.com>
2014-06-16 13:37:46 +0200

    Add some php files

--- public_html/menu.php
+++ public_html/menu.php
@@ -1,11 +1,13 @@
 <div id="menu">
 	<ul id="navigation">
-		<li><a href="index.php" title="Home">Home</a></li>
-		<li><a href="search-bydate.php" title="Search by date">Chercher par date</a></li>
-		<li><a href="search-byuser.php" title="Search by user">Chercher par utilisateur</a></li>
-		<li><a href="stats.php" title="Stats">Stats</a></li>
-		<li><a href="#" title="Help">Aide</a></li>
-		<li><a href="#" title="About">A propos</a></li>
+		<li><a href="index.php" title="Accueil">Accueil</a></li>
+		<li><a href="search-bydate.php" title="Chercher par date">Chercher par date</a></li>
+		<li><a href="search-byuser.php" title="Chercher par utilisateur">Chercher par utilisateur</a></li>
+		<li><a href="search-bysite.php" title="Chercher par site">Chercher par site</a></li>
+		<li><a href="search-bykeyword.php" title="Chercher par mot clé">Chercher par mot clé</a></li>
+		<li><a href="stats.php" title="TOP20">TOP20</a></li>
+		<li><a href="help.php" title="Aide">Aide</a></li>
+		<li><a href="about.php" title="A propos">A propos</a></li>
 	</ul>
 </div>
 
