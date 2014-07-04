6ac9b18b56755298b4a11ff910dc16811df9a8eb
Adrien_D <aaaaadrien@users.noreply.github.com>
2014-07-05 00:08:04 +0200

    Add some lines to encode all in UTF8

--- public_html/config.php
+++ public_html/config.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-Type: text/html; charset=UTF-8');
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=marjo21mlo', 'root', 'root');
-
+$pdo->exec("SET CHARACTER SET utf8");                                                                                                                        
 ?>
