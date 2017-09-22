ae9779a35d031717aac220ec9ca313d5f47a70c3
Adrien_D <ghbdtn@openmailbox.org>
2017-09-22 19:00:55 +0200

    Add backup script

---
+++ sauvegarde.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+ 
+echo On definit les variables systeme
+ 
+dte=$(date +%Y-%m-%d--%H-%M-%S)
+path=`dirname $0`
+pathsav="$path/sauvegarde"
+
+ficrap="$pathsav/sauvegarde-$dte-rapport.log"
+
+dbhost="localhost"
+dbuser="marjo21"
+dbpasswd="marjo21"
+dbname="marjo21"
+
+echo Variables definies.
+
+echo ""
+
+mkdir -p "$pathrap" 2>/dev/null
+
+echo "RAPPORT SAUVEGARDE $site du $dte" > "$ficrap"
+
+echo Iniatalisation du script >> "$ficrap"
+cd "$path"
+mkdir -p "$pathsav" 2>/dev/null
+echo Script initialise >> "$ficrap"
+echo "" >> "$ficrap"
+
+echo On lance la sauvegarde de la base de donnees >> "$ficrap"
+
+mysqldump -u "$dbuser" -p"$dbpasswd" -h "$dbhost" "$dbname" > "$pathsav/$dbname-$dte.sql"
+
+if [ $? -eq 0 ]
+then
+        echo Base de donnees sauvegardee. >> "$ficrap"
+else
+        echo Echec de la sauvegarde de la base de données >> "$ficrap"
+        exit 4
+fi
+
+echo "" >> "$ficrap"
+
+echo On compresse la base de données. >> "$ficrap"
+
+bzip2 -z  "$pathsav/$dbname-$dte.sql"
+
+if [ $? -eq 0 ]
+then
+        echo Base de donnees compressée. >> "$ficrap"
+else
+        echo Echec de la compression de la base de données >> "$ficrap"
+        exit 5
+fi
+
+echo "" >> "$ficrap"
+
+echo SAUVEGARDE TERMINEE >> "$ficrap
